40e3c01b51ca3c798dfc5211b9bc732b3318f203
刘旋尧 <liuxy@weipaitang.com>
2022-12-29 11:33:47 +0800

    fix

--- jwt/context.go
+++ jwt/context.go
@@ -12,28 +12,39 @@ var ErrNotLogin = errors.New("not logged in")
 
 type authKey struct{}
 
-func WithContext(ctx context.Context, claims jwt.Claims) context.Context {
-	return context.WithValue(ctx, authKey{}, claims)
+func WithContext(ctx context.Context, token *jwt.Token) context.Context {
+	return context.WithValue(ctx, authKey{}, token)
 }
 
-func FromContext(ctx context.Context) (jwt.Claims, bool) {
-	rs, ok := ctx.Value(authKey{}).(jwt.Claims)
+func FromContext(ctx context.Context) (*jwt.Token, bool) {
+	rs, ok := ctx.Value(authKey{}).(*jwt.Token)
 	return rs, ok
 }
 
 func GetUserID(ctx context.Context) (int, error) {
-	claim, ok := FromContext(ctx)
+	token, ok := FromContext(ctx)
 	if !ok {
 		return 0, ErrNotLogin
 	}
-	mapClaim, ok := claim.(jwt.MapClaims)
+
+	mapClaim, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
 		return 0, ErrNotLogin
 	}
+
 	id, ok := mapClaim["jti"]
 	if !ok {
 		return 0, ErrNotLogin
 	}
 
 	return strconv.Atoi(id.(string))
+}
+
+func GetAccessToken(ctx context.Context) (string, error) {
+	token, ok := FromContext(ctx)
+	if !ok {
+		return "", ErrNotLogin
+	}
+
+	return token.Raw, nil
 }
