0c96ed53ca1722c9d11dd77a5f80952c9c9702f8
刘旋尧 <liuxy@weipaitang.com>
2023-01-03 19:30:20 +0800

    fix

--- conf/conf.go
+++ conf/conf.go
@@ -1,18 +1,20 @@
 package conf
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
 
 type Options struct {
-	Path string `json:"path"`
+	Path []string `json:"path"`
 }
 
 type option func(*Options)
 
-func WithPath(path string) option {
+func WithPath(path ...string) option {
 	return func(o *Options) { o.Path = path }
 }
 
@@ -22,8 +24,14 @@ func Setup(options ...option) error {
 	for _, opt := range options {
 		opt(ops)
 	}
+	for _, path := range ops.Path {
+		viper.AddConfigPath(path)
+	}
 
-	viper.AddConfigPath(ops.Path)
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.String())
+	})
+	viper.WatchConfig()
 
 	err := viper.ReadInConfig()
 	if err != nil {
